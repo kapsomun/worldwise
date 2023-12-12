9bce96a9d2b9407f816eecc9488ba69afc279421
igor-front <parsival312@mail.ru>
2023-12-12 15:28:38 +0200

    feat: add page navigation

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
 import styles from "./Homepage.module.css";
 
@@ -16,6 +17,7 @@ export default function Homepage() {
           of. Never forget your wonderful experiences, and show your friends how
           you have wandered the world.
         </h2>
+        <Link className={styles.ctaLink} to='/app'>go to app</Link>
       </section>
     </main>
   );
